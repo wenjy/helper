a9c5d1296320188d4a2cbf9c10397c9b4f202aed
wenjiangyi <504038824@qq.com>
2022-03-21 10:51:54 +0800

    add SocketFD func

---
+++ conn.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"net"
+	"reflect"
+	"syscall"
+)
+
+func SocketFD(conn net.Conn) int {
+	if con, ok := conn.(syscall.Conn); ok {
+		raw, err := con.SyscallConn()
+		if err != nil {
+			return 0
+		}
+		sfd := 0
+		raw.Control(func(fd uintptr) {
+			sfd = int(fd)
+		})
+		return sfd
+	}
+
+	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
+	fdVal := tcpConn.FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+	return int(pfdVal.FieldByName("Sysfd").Int())
+}
